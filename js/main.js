1d7f2e3f8d8548eb556abffefd296cdcdfa20981
Alienah <aidalbla@gmail.com>
2017-11-25 01:54:04 +0100

    Put rules in js as comments, because prompts are showing up all the time

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //Experience
+/*
 var jobName = document.querySelector('#job');
 var jobInput = prompt('Introduce el nombre del puesto de trabajo');
 jobName.innerHTML = jobInput;
@@ -8,3 +9,4 @@ jobName.innerHTML = jobInput;
 var companyName = document.querySelector('#company');
 var companyInput = prompt('Introduce el nombre de la empresa');
 companyName.innerHTML = companyInput;
+*/
