78ed3f114ebe208bbaa0fe8a091b2b13451f3c23
Alienah <aidalbla@gmail.com>
2017-11-25 01:45:25 +0100

    Add first rules js in experience. First try

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//Experience
+var jobName = document.querySelector('#job');
+var jobInput = prompt('Introduce el nombre del puesto de trabajo');
+jobName.innerHTML = jobInput;
+
+var companyName = document.querySelector('#company');
+var companyInput = prompt('Introduce el nombre de la empresa');
+companyName.innerHTML = companyInput;
