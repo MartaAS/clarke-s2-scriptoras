f47269d516763999c9b1110c8932dab92bf19068
Marta Aguilar <martaguilar.soto@gmail.com>
2017-11-29 13:42:40 +0100

    file principal image, buttom file imagen

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 'use strict';
+//prueba cargar imagen
+function archivo(evt) {
+      var files = evt.target.files; // FileList object
+
+        //Obtenemos la imagen del campo "file".
+      for (var i = 0, f; f = files[i]; i++) {
+           //Solo admitimos imágenes.
+           if (!f.type.match('image.*')) {
+                continue;
+           }
+
+           var reader = new FileReader();
+
+           reader.onload = (function(theFile) {
+               return function(e) {
+               // Creamos la imagen.
+                      document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+               };
+           })(f);
+
+           reader.readAsDataURL(f);
+       }
+}
+
+      document.getElementById('files').addEventListener('change', archivo, false);
+	//fin prueba cargar imagen
+
 //Datos principales
 var nombreIntroducido = prompt("¿Cuál es tu nombre");
 document.getElementById('nombreformulario').value=nombreIntroducido;
