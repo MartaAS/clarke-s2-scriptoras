f736bd8759d88bc4e1a623d633e832726d1454cf
Cris <crisgalasanchez@gmail.com>
2017-11-29 13:43:14 +0100

    load an image qr

--- js/main.js
+++ js/main.js
@@ -148,20 +148,26 @@ function setSocialMedia(value, net){
 */
 /*qr*/
 function archivo(evt) {
-      var files = evt.target.files; // FileList object
-        //Obtenemos la imagen del campo "file".
-      for (var i = 0, f; f = files[i]; i++) {
-           //Solo admitimos imágenes.
-           if (!f.type.match('image.*')) {
-                continue;
-           }
-           var reader = new FileReader();
-           reader.onload = (function(theFile) {
-               return function(e) {
-               // Creamos la imagen.
-                      document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
-               };
-           })(f);
-           reader.readAsDataURL(f);
-       }
-      document.getElementById('files').addEventListener('change', archivo, false);
+                  var files = evt.target.files; // FileList object
+
+                  // Obtenemos la imagen del campo "file".
+                  for (var i = 0, f; f = files[i]; i++) {
+                    //Solo admitimos imágenes.
+                    if (!f.type.match('image.*')) {
+                        continue;
+                    }
+
+                    var reader = new FileReader();
+
+                    reader.onload = (function(theFile) {
+                        return function(e) {
+                          // Insertamos la imagen
+                         document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+                        };
+                    })(f);
+
+                    reader.readAsDataURL(f);
+                  }
+              }
+
+              document.getElementById('files').addEventListener('change', archivo, false);
